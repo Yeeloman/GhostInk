1198dd1bac4906d9505e71e2c9d8b7bd8959659a
Yeeloman <yami.onlyme@gmail.com>
2024-11-04 02:08:25 +0100

    coloring the filename output

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="GhoslInk",
-    version="0.0.2",
+    version="0.0.5",
     packages=find_packages(),
     author="Yeeloman",
     author_email="yami.onlyme@gmail.com",
