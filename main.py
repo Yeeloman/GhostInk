dc957bb1b8fe2389bcde1dc6e71159624f3dc3e7
Yeeloman <yami.onlyme@gmail.com>
2024-11-08 20:00:20 +0100

    update

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from ghostink import GhostInk
 
 ink = GhostInk()
 shade = ink.get_shades()
-ink.drop("test", shade.TODO, ['testing'])
+ink.drop('this an info ask', shade.INFO, ['database', 'api'])
 ink.whisper()
